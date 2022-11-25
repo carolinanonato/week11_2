fb59f0a489e5f9144493fb9c40e63e6a34e38c3b
Carol Nonato <csnonato@gmail.com>
2022-11-25 20:50:16 +0000

    added code from professor's repo

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# week11_2
+# acs730_week11
+
+[![tfsec](https://github.com/igeiman13/acs730_week11/actions/workflows/staging_tfsec.yml/badge.svg)](https://github.com/igeiman13/acs730_week11/actions/workflows/staging_tfsec.yml)
---
+++ prod/main.tf
@@ -0,0 +1,47 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.40.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.4.3"
+    }
+  }
+  required_version = ">= 1.0.0"
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "random_pet" "sg" {}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+
+resource "aws_security_group" "web-sg" {
+  name = "${random_pet.sg.id}-sg"
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["1.2.3.4/32"]
+  }
+}
+
